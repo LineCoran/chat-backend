633ac86cb2bfc09b399fb033f49e4d6ab0ba74ff
LineCoran <vppisya@gmail.com>
2023-03-08 20:19:40 +0300

    feat: upload image

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import likeRouters from './routes/likes.js';
 import authRouters from './routes/auth.js';
 import cors from "cors";
 import cookieParser from 'cookie-parser';
+import multer from 'multer';
 
 const app = express();
 const PORT = 4000;
@@ -20,6 +21,22 @@ app.use(cors({
   origin: "http://localhost:3000"
 }));
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, '../chat/public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+  }
+});
+
+const upload = multer({ storage: storage} );
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+})
 
 app.use('/users', userRouters);
 app.use('/posts', postRouters);
